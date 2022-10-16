3561eb1265cff956896f5b7950a99b0e2e87179c
Kimu-Nowchira <hon20ke@naver.com>
2022-10-16 14:23:33 +0900

    ✨ module that can convert blocks to string

---
+++ client/src/utils/blocksToString.ts
@@ -0,0 +1,18 @@
+import { Block } from "../types"
+
+const blocksToString = (blocks: Block[]): string => {
+  let content = ""
+
+  let index = 0
+  for (const block of blocks) {
+    if (index && blocks[index - 1].blockType !== block.blockType)
+      content += "\n"
+
+    content += block.content + "\n"
+
+    index++
+  }
+  return content
+}
+
+export default blocksToString
