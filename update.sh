bac5b8345450d182d5e09632b046eb8238c3bb22
Pariring <paring@pikokr.dev>
2023-01-12 11:09:43 +0900

    feat: update script

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+
+docker rm -f muvel-server
+echo Deleted previous container
+
+docker build -t muvel-server .
+
+docker run -it -d --name muvel-server -v $PWD/data:/app/server/.tmp -p 127.0.0.1:1338:1337 muvel-server
+
+echo Done!
