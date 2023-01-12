38a3be8e5a015b9343a71b913ed5893b7b5e2703
Pariring <paring@pikokr.dev>
2023-01-12 11:15:08 +0900

    fix: env loading

--- update.sh
+++ update.sh
@@ -2,13 +2,11 @@
 
 set -x
 
-. ./env.sh
-
 docker rm -f muvel-server
 echo Deleted previous container
 
 docker build -t muvel-server .
 
-docker run -it -d --name muvel-server -v $PWD/data:/app/server/.tmp -p 127.0.0.1:1338:1337 muvel-server
+docker run -it -d --name muvel-server -v $PWD/data:/app/server/.tmp -p 127.0.0.1:1338:1337 --env-file .env muvel-server
 
 echo Done!
