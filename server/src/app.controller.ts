4f6aab9712fa4859d284e055729fe6d3b536fdbc
Kimu-Nowchira <hon20ke@naver.com>
2023-01-16 03:08:09 +0900

    🔥 remove test code

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,22 +1,4 @@
-import { Controller, Get, Param, Query } from "@nestjs/common"
-import { AppService } from "./app.service"
+import { Controller } from "@nestjs/common"
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello()
-  }
-
-  @Get("dev")
-  queryString(@Query("id") id: string): string {
-    return id
-  }
-
-  @Get("paring/:id")
-  param(@Param("id") id: string): string {
-    return id
-  }
-}
+export class AppController {}
