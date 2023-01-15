2a6229403036dd9611ecc902bf535bc1c14fb4a6
Kimu-Nowchira <hon20ke@naver.com>
2023-01-16 01:01:17 +0900

    🐛 fix port error

--- update.sh
+++ update.sh
@@ -7,6 +7,6 @@ echo Deleted previous container
 
 docker build -t muvel-server .
 
-docker run -it -d --name muvel-server -v $PWD/data:/app/server/.tmp -p 127.0.0.1:1338:1337 --env-file .env muvel-server
+docker run -it -d --name muvel-server -v $PWD/data:/app/server/.tmp -p 127.0.0.1:1338:3000 --env-file .env muvel-server
 
 echo Done!
