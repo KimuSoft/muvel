feed1acfb936094fd9d30dfe1896dfbe7cc8f915
Kimu-Nowchira <keleekini@gmail.com>
2023-07-26 08:10:44 +0900

    🐛 fix: 유저 정보를 불러오지 못했을 때 발생하는 오류 예외처리

--- client/src/layouts/AppLayout.tsx
+++ client/src/layouts/AppLayout.tsx
@@ -3,18 +3,35 @@ import { Outlet } from "react-router-dom"
 import GlobalContext from "../context/GlobalContext"
 import { api } from "../utils/api"
 import { User } from "../types/user.type"
-import { Center, Spinner } from "@chakra-ui/react"
+import { Center, Spinner, useToast } from "@chakra-ui/react"
 import { AiOutlineLoading3Quarters } from "react-icons/ai"
+import { AxiosError } from "axios"
 
 export const AppLayout: React.FC = () => {
   const [loading, setLoading] = React.useState(true)
   const [user, setUser] = React.useState<User | null>(null)
 
+  const toast = useToast()
+
   React.useEffect(() => {
     ;(async () => {
-      const { data } = await api.get("/users/me")
+      let user: User | null = null
+      try {
+        user = (await api.get("/users/me")).data
+      } catch (e) {
+        if (!(e instanceof AxiosError)) throw e
+        switch (e.response?.status) {
+          case 401:
+            break
+          default:
+            toast({
+              title: "알 수 없는 오류가 발생했습니다",
+              description: e.message,
+            })
+        }
+      }
 
-      setUser(data)
+      setUser(user)
     })().finally(() => setLoading(false))
   }, [])
 
