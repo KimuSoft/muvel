3aaf0ea09bf2a603839e1114ca623952f76a1c3a
kimustory <hon20ke@naver.com>
2025-04-22 15:50:11 +0900

    fix: api 수정

--- packages/client/app/utils/frontApi.ts
+++ packages/client/app/utils/frontApi.ts
@@ -1,5 +1,9 @@
 import axios from "axios"
 
 export const frontApi = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE,
+  baseURL:
+    (typeof window === "undefined"
+      ? process.env.VITE_API_BASE
+      : import.meta.env.VITE_API_BASE) || "/api",
+  withCredentials: true,
 })
